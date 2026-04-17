334c2a759bdc9e4f7a9f827f3e2878e56695a1f1
Vennacheti Jayanthi <jayanthivennacheti24@gmail.com>
2026-04-17 22:10:03 +0530

    updated app message for CI/CD test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello DevOps UPDATED VERSION 🚀 " + socket.gethostname()
+    return "Hello DevOps 🚀 CI/CD Working" + socket.gethostname()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
