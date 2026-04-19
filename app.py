e3c8fa4aae8677f75664caef564e898c41ccfbfb
Vennacheti Jayanthi <jayanthivennacheti24@gmail.com>
2026-04-19 11:28:08 +0530

    ready for deployment

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask
+import random
+from flask import Flask, render_template
 import socket
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello DevOps 🚀 CI/CD Working" + socket.gethostname()
+    return render_template("index.html")
+
+
+
+@app.route("/dashboard")
+def dashboard():
+    fake_pods = ["pod-1", "pod-2", "pod-3"]
+    hostname = random.choice(fake_pods)
+
+    cpu = random.randint(10, 90)
+    requests = random.randint(100, 1000)
+
+    return render_template("dashboard.html",
+                           hostname=hostname,
+                           cpu=cpu,
+                           requests=requests)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
